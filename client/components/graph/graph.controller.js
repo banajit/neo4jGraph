f634dcb2fdf11dcd488edd9e7666eb8fd83c9d03
Banajit <banajit@visibleaccent.com>
2016-11-23 18:14:59 +0530

    Tooltip Implementation

--- client/components/graph/graph.controller.js
+++ client/components/graph/graph.controller.js
@@ -5,7 +5,7 @@
   function graphCtrl($scope, CONSTANTS) {
    CONSTANTS.getConfig().success(function (data) {
      var neo4jConfig = data.neo4jConfig;
-     var query = 'MATCH (n:SystemName) MATCH (n)-[r]->(m) RETURN n,r,m LIMIT 20';
+     var query = 'MATCH (n:SystemName) MATCH (n)-[r]->(m) RETURN n,r,m LIMIT 25';
      var graphMetaInfo = {serverConfig:neo4jConfig, neo4jQuery:query};
      $scope.$broadcast('renderGraph', graphMetaInfo);
    });
--- client/components/graph/graph.directive.js
+++ client/components/graph/graph.directive.js
@@ -10,7 +10,7 @@ angular.module('neo4jApp')
       controller: 'graphCtrl',
       link: function (scope) {
         var sigmaInstance, loaded = 0;
-        //sigma.utils.pkg('sigma.canvas.nodes');
+        sigma.utils.pkg('sigma.canvas.nodes');
         sigma.canvas.nodes.image = (function() {
           var _cache = {},
               _loading = {},
@@ -90,11 +90,11 @@ angular.module('neo4jApp')
         })();
         //Listen for graph render event
         scope.$on('renderGraph', function (event, data) {
-           renderGraph(data);
+           renderSigmaInstance(data);
         });
 
         function renderSigmaInstance(graphMeta) {
-          var graph = {
+          var graph1 = {
             nodes: [],
             edges: [],
             urls : [
@@ -117,172 +117,165 @@ angular.module('neo4jApp')
               { url: graphMeta.serverConfig.serverUrl, user: graphMeta.serverConfig.user, password: graphMeta.serverConfig.password },
               graphMeta.neo4jQuery,
             function(graph) {
-              graph.nodes.forEach(function(node) {
-                node.id = node.id;
-                node.label = node.neo4j_data.SystemName;
-                node.size = 8;
-                node.x = Math.floor((Math.random() * 100) + 1);
-                node.y = Math.floor((Math.random() * 100) + 1);
-                node.type = 'image';
-                node.url = 'assets/images/img1.png';
-                node.color = '#E2E2E2';
-              });
-              graph.edges.forEach(function(edge, key) {
-                edge.id = edge.id;
-                edge.label = edge.label;
-                edge.source = edge.source;
-                edge.target = edge.target;
-                edge.type = 'arrow';
-                edge.count = key;
-                edge.color = '#ccc';
-                edge.hover_color = '#000';
-              });
-               var sigmaInstance = new sigma({
-                 graph: graph,
-                 renderer: {
-                   container: document.getElementById('neo4jgraph'),
-                   type: 'canvas',
-                 }
-               });
-
-              sigmaInstance.settings({
-                autoCurveSortByDirection: true,
-                minNodeSize: 3,
-                maxNodeSize: 10,
-                minEdgeSize: 3,
-                maxEdgeSize: 3,
-                defaultLabelColor: '#000',
-                //defaultLabelSize: 16,
-                defaultEdgeLabelColor: '#014AB6',
-                //defaultEdgeLabelSize: 16,
-                singleHover: true,
-                enableEdgeHovering: true,
-                edgeHoverColor: 'edge',
-                defaultEdgeHoverColor: '#000',
-              });
-              //bind the events
-             /* sigmaInstance.bind('clickNode', function (e) {
-                console.log(e);
-              });*/
-              sigmaInstance.bind('hovers', function (e) {
-                var adjacentNodes = {},
-                      adjacentEdges = {};
-
-                if (!e.data.enter.nodes.length) {
-                  //on mouseout
-                  sigmaInstance.graph.edges().forEach(function (edge) {
-                      edge.color = '#ccc';
-                      edge.hidden = false;
-                  });
-                  sigmaInstance.graph.nodes().forEach(function (node) {
-                      node.hidden = false;
-                  });
-                }
-                else {
-                  //on mousein
-                  // Get adjacent nodes:
-                  e.data.enter.nodes.forEach(function(node) {
-                    adjacentNodes[node.id] = node.id;
-                  });
-                  var neighborNodes = {};
-                  sigmaInstance.graph.edges().forEach(function (edge) {
-                      if (adjacentNodes[edge.source] !== undefined || adjacentNodes[edge.target] !== undefined) {
-                        edge.color = '#000';
-                        edge.hidden = false;
-                        neighborNodes[edge.source] = edge.source;
-                        neighborNodes[edge.target] = edge.target;
-                      }
-                      else {
-                         //edge.hidden = true;
-                      }
-                  });
-                  /*sigmaInstance.graph.nodes().forEach(function (node) {
-                      if (neighborNodes[node.id] !== undefined) {
-                        node.hidden = false;
-                      }
-                      else {
-                         node.hidden = true;
-                      }
-                  });*/
-                }
-                sigmaInstance.refresh();
-              });
-
-              //Show tooltip
-              var config_tooltip = {
-                node: [{
-                  show: 'clickNode',
-                  cssClass: 'sigma-tooltip',
-                  position: 'top',
-                  autoadjust: true,
-                  renderer: function(node) {
-                    var customTemplate = '<md-card class="entity"><div class="card-info">';
-                    customTemplate += '<h2 class="card-header">' + node.label + '</h2>';
-                    customTemplate += '<ul>';
-                    angular.forEach(node.neo4j_data, function(value, key){
-                      customTemplate += '<li><span class="li-title">' + key + '</span><span class="li-value">' + value + '</span></li>';
-                    });
-                    customTemplate += '</ul></div></md-card>';
-                    return Mustache.render(customTemplate, node);
+              graph1.urls.forEach(function(url) {
+                sigma.canvas.nodes.image.cache(
+                  url,
+                  function() {
+                    if (++loaded === graph1.urls.length)
+                      // Instantiate sigma:
+                       sigmaInstance = createSigmaInstance(graph);
                   }
-                }],
-                edge: [{
-                  show: 'clickEdge',
-                  cssClass: 'sigma-tooltip',
-                  position: 'top',
-                  autoadjust: true,
-                  renderer: function(edge) {
-                    console.log(edge);
-                    var customTemplate = '<md-card class="entity"><div class="card-info">';
-                    customTemplate += '<h2 class="card-header">' + edge.label + '</h2>';
-                    customTemplate += '<ul>';
-                    angular.forEach(edge.neo4j_data, function(value, key){
-                      customTemplate += '<li><span class="li-title">' + key + '</span><span class="li-value">' + value + '</span></li>';
-                    });
-                    customTemplate += '</ul></div></md-card>';
-                    return Mustache.render(customTemplate, edge);
-                  }
-                }]
-              };
-
-              // Instanciate the tooltips plugin with a Mustache renderer for node tooltips:
-              var tooltips = sigma.plugins.tooltips(sigmaInstance, sigmaInstance.renderers[0], config_tooltip);
-
-
-              //nonoverlaping node config
-              var config = {
-                nodeMargin: 10,
-                scaleNodes: 2
-              };
-              var listener = sigmaInstance.configNoverlap(config);
-              sigmaInstance.startNoverlap();
-              sigma.canvas.edges.autoCurve(sigmaInstance);
-
-              //make nodes draggable
-              var activeState = sigma.plugins.activeState(sigmaInstance);
-              var renderer = sigmaInstance.renderers[0];
-              var dragListener = new sigma.plugins.dragNodes(sigmaInstance, renderer, activeState);
-              sigmaInstance.refresh();
+                );
+              });
             }
           );
         }
+        //create sigma instance
+        function createSigmaInstance(graph) {
+          var sigmaInstance = new sigma({
+            graph: graph,
+            renderer: {
+              container: document.getElementById('neo4jgraph'),
+              type: 'canvas',
+            }
+          });
+          graph.nodes.forEach(function(node) {
+            node.id = node.id;
+            node.label = node.neo4j_data.SystemName;
+            node.size = 8;
+            node.x = Math.floor((Math.random() * 100) + 1);
+            node.y = Math.floor((Math.random() * 100) + 1);
+            node.type = 'image';
+            node.url = 'assets/images/img1.png';
+            node.color = '#68BDF6';
+          });
+          graph.edges.forEach(function(edge, key) {
+            edge.id = edge.id;
+            edge.label = edge.label;
+            edge.source = edge.source;
+            edge.target = edge.target;
+            edge.type = 'arrow';
+            edge.count = key;
+            edge.color = '#ccc';
+            edge.hover_color = '#000';
+          });
+          sigmaInstance.settings({
+            autoCurveSortByDirection: true,
+            minNodeSize: 3,
+            maxNodeSize: 12,
+            minEdgeSize: 1,
+            maxEdgeSize: 1,
+            defaultLabelColor: '#000',
+            //defaultLabelSize: 16,
+            defaultEdgeLabelColor: '#014AB6',
+            //defaultEdgeLabelSize: 16,
+            singleHover: true,
+            enableEdgeHovering: true,
+            edgeHoverColor: 'edge',
+            defaultEdgeHoverColor: '#000'
+          });
+          //bind the events
+          sigmaInstance.bind('hovers', function (e) {
+            var adjacentNodes = {},
+                  adjacentEdges = {};
 
-        // Render Graph
-        function renderGraph(graphMeta) {
-          // Then, wait for all images to be loaded before instanciating sigma:
-          renderSigmaInstance(graphMeta);
-          /*graph.urls.forEach(function(url) {
-            sigma.canvas.nodes.image.cache(
-              url,
-              function() {
-                if (++loaded === graph.urls.length)
-                  // Instantiate sigma:
-                   renderSigmaInstance(graph);
+            if (!e.data.enter.nodes.length) {
+              //on mouseout
+              sigmaInstance.graph.edges().forEach(function (edge) {
+                  edge.color = '#ccc';
+                  edge.hidden = false;
+              });
+              sigmaInstance.graph.nodes().forEach(function (node) {
+                  node.hidden = false;
+              });
+            }
+            else {
+              //on mousein
+              // Get adjacent nodes:
+              e.data.enter.nodes.forEach(function(node) {
+                adjacentNodes[node.id] = node.id;
+              });
+              var neighborNodes = {};
+              sigmaInstance.graph.edges().forEach(function (edge) {
+                  if (adjacentNodes[edge.source] !== undefined || adjacentNodes[edge.target] !== undefined) {
+                    edge.color = '#000';
+                    edge.hidden = false;
+                    neighborNodes[edge.source] = edge.source;
+                    neighborNodes[edge.target] = edge.target;
+                  }
+                  else {
+                     //edge.hidden = true;
+                  }
+              });
+              /*sigmaInstance.graph.nodes().forEach(function (node) {
+                  if (neighborNodes[node.id] !== undefined) {
+                    node.hidden = false;
+                  }
+                  else {
+                     node.hidden = true;
+                  }
+              });*/
+            }
+            sigmaInstance.refresh();
+          });
+
+          //Show tooltip
+          var config_tooltip = {
+            node: [{
+              show: 'clickNode',
+              cssClass: 'sigma-tooltip',
+              position: 'top',
+              autoadjust: true,
+              renderer: function(node) {
+                var customTemplate = '<md-card class="entity"><div class="card-info">';
+                customTemplate += '<h2 class="card-header" title="' + node.label + '">' + node.label + '</h2>';
+                customTemplate += '<ul>';
+                angular.forEach(node.neo4j_data, function(value, key){
+                  customTemplate += '<li><span class="li-title">' + key + '</span><span title="' + value + '" class="li-value">' + value + '</span></li>';
+                });
+                customTemplate += '</ul></div></md-card>';
+                return Mustache.render(customTemplate, node);
               }
-            );
-          });*/
-        }
+            }],
+            edge: [{
+              show: 'clickEdge',
+              cssClass: 'sigma-tooltip',
+              position: 'top',
+              autoadjust: true,
+              renderer: function(edge) {
+                console.log(edge);
+                var customTemplate = '<md-card class="entity"><div class="card-info">';
+                customTemplate += '<h2 class="card-header">' + edge.label + '</h2>';
+                customTemplate += '<ul>';
+                angular.forEach(edge.neo4j_data, function(value, key){
+                  customTemplate += '<li><span uib-tooltip="After today restriction" class="li-title">' + key + '</span><span class="li-value">' + value + '</span></li>';
+                });
+                customTemplate += '</ul></div></md-card>';
+                return Mustache.render(customTemplate, edge);
+              }
+            }]
+          };
 
+          // Instanciate the tooltips plugin with a Mustache renderer for node tooltips:
+          var tooltips = sigma.plugins.tooltips(sigmaInstance, sigmaInstance.renderers[0], config_tooltip);
+
+
+          //nonoverlaping node config
+          var config = {
+            nodeMargin: 10,
+            scaleNodes: 2
+          };
+          var listener = sigmaInstance.configNoverlap(config);
+          sigmaInstance.startNoverlap();
+          sigma.canvas.edges.autoCurve(sigmaInstance);
+
+          //make nodes draggable
+          var activeState = sigma.plugins.activeState(sigmaInstance);
+          var renderer = sigmaInstance.renderers[0];
+          var dragListener = new sigma.plugins.dragNodes(sigmaInstance, renderer, activeState);
+          sigmaInstance.refresh();
+          return sigmaInstance;
+        }
       }
     };
   });
