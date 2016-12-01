44699120f42f008050ae88d795d538350b322f42
Banajit <banajit@visibleaccent.com>
2016-12-01 17:26:42 +0530

    updated layout

--- client/components/graph/graph.controller.js
+++ client/components/graph/graph.controller.js
@@ -5,9 +5,9 @@
   function graphCtrl($scope, CONSTANTS) {
    CONSTANTS.getConfig().success(function (data) {
      var neo4jConfig = data.neo4jConfig;
-     var query = 'MATCH (n:SystemName) MATCH (n)-[r]->(m) RETURN n,r,m';
-     //var query = 'MATCH (n) MATCH ()-[r]->() RETURN n,r';
-     var graphMetaInfo = {serverConfig:neo4jConfig, neo4jQuery:query};
+     //var query = 'MATCH (n:SystemName) MATCH (n)-[r]->(m) RETURN n,r,m';
+     var query = 'MATCH (n) MATCH ()-[r]->() RETURN n,r';
+     //var graphMetaInfo = {serverConfig:neo4jConfig, neo4jQuery:query};
      CONSTANTS.setStateVariable('serverConfig', neo4jConfig);
      $scope.$broadcast('renderGraph', graphMetaInfo);
    });
